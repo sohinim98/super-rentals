ca79815a4b08e0e96b6de0a9861e35535ddadf08
Sohini Mukherjee <sohini.mukherjee@pncl.tech>
2021-09-01 14:43:00 -0400

    Added automated testing.

---
+++ tests/acceptance/super-rentals-test.js
@@ -0,0 +1,56 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+/** Here, we are writing the tests in a framework called QUnit,
+ * which is where the functions module, test and assert come from
+ * We also have additional helpers like click, visit, and currentURL
+ * provided by the @ember/test-helpers package.
+ *
+ * http://localhost:7357/
+ */
+
+module('Acceptance | super rentals', function (hooks) {
+  setupApplicationTest(hooks);
+
+  // instruct the test robot to navigate to the / URL of our app
+  // landing page
+  test('visiting /', async function (assert) {
+    // wait until the page completely finishes loading before moving on to the next step.
+    // almost always use async & await visit as a pair
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+    // look inside the tag with the jumbo class for an <a> tag with the button class
+    // same as css
+    assert.dom('.jumbo a.button').hasText('About Us');
+    // click
+    await click('.jumbo a.button');
+    assert.equal(currentURL(), '/about');
+  });
+  // about
+  test('visiting /about', async function (assert) {
+    await visit('/about');
+
+    assert.equal(currentURL(), '/about');
+    assert.dom('h2').hasText('About Super Rentals');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+  });
+  // contact
+  test('visiting /getting-in-touch', async function (assert) {
+    await visit('/getting-in-touch');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('.jumbo a.button').hasText('About');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
+  });
+});
