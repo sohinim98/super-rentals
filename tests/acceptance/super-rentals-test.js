7d7b1260e399bc707508793db99336f63d112bf3
Sohini Mukherjee <sohini.mukherjee@pncl.tech>
2021-09-01 15:12:32 -0400

    Nav, components.

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -21,6 +21,8 @@ module('Acceptance | super rentals', function (hooks) {
     await visit('/');
 
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Welcome to Super Rentals!');
     // look inside the tag with the jumbo class for an <a> tag with the button class
     // same as css
@@ -34,6 +36,8 @@ module('Acceptance | super rentals', function (hooks) {
     await visit('/about');
 
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('About Super Rentals');
 
     assert.dom('.jumbo a.button').hasText('Contact Us');
@@ -46,11 +50,36 @@ module('Acceptance | super rentals', function (hooks) {
     await visit('/getting-in-touch');
 
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('.jumbo a.button').hasText('About');
     await click('.jumbo a.button');
 
     assert.equal(currentURL(), '/about');
+  });
+  /** We could write a component test for the <NavBar> by itself,
+   * like we just did for the <Jumbo> component.
+   * However, since the job of <NavBar> is to navigate us around the app,
+   * it would not make a lot of sense to test this particular component in isolation.
+   * So, let's go back to writing some acceptance tests!
+   */
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
   });
 });
